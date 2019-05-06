7d405184f4a19527cd9eb17e05d146bda84f34a5
mpaauw <matt.paauw@gmail.com>
2019-05-06 08:48:00 -0700

    Add solution for 21.

---
+++ 21-merge-two-sorted-lists.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+        {
+            return l2;
+        }
+        else if(l2 == null)
+        {
+            return l1;
+        }
+        if(l1.val <= l2.val)
+        {
+            l1.next = MergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = MergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+public class ListNode{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) 
+    {
+        val = x;
+    }
+}
