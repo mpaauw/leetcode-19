242a3c79cc3810e87e5a11812257f338806b44bd
mpaauw <matt.paauw@gmail.com>
2019-05-09 19:09:31 -0700

    Add full solution for 973.

--- 973-k-closest-points-to-origin.cs
+++ 973-k-closest-points-to-origin.cs
@@ -2,32 +2,26 @@ public class Solution
 {
     public int[][] KClosest(int[][] points, int K)
     {
-
-        // use sorted set to store euclidean distances:
         int[][] result = new int[K][];
-        Dictionary<int, double> dict = new Dictionary<int, double>(); // <index of point in points, distance>
-
-        // calculate euclidean distances for each point, store in set:
-        for(int i = 0; i < points.Length; i++)
+        double[] distances = new double[points.Length];
+        for (int i = 0; i < points.Length; i++)
         {
             int[] point = points[i];
-
             double distance = Math.Sqrt(Math.Pow(0 - point[0], 2) + Math.Pow(0 - point[1], 2));
-
-            dict.Add(i, distance);
+            distances[i] = distance;
         }
-
-        var sortedValues = dict.Values.ToArray();
-        Array.Sort(sortedValues);
-
-        for (int i = 0; i < K; i++)
+        Array.Sort(distances);
+        double distanceThreshold = distances[K - 1];
+        int iter = 0;
+        for (int i = 0; i < points.Length; i++)
         {
-            int indexKey = dict.FirstOrDefault(x => x.Value == sortedValues[i]).Key;
-
-            result[i] = points[indexKey];
-            dict.Remove(indexKey); // don't double-tap points in result
+            int[] point = points[i];
+            double distance = Math.Sqrt(Math.Pow(0 - point[0], 2) + Math.Pow(0 - point[1], 2));
+            if (distance <= distanceThreshold)
+            {
+                result[iter++] = point;
+            }
         }
-
         return result;
     }
 }
