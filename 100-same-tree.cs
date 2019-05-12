25453f54c078b60313097879088c7d15542fe96e
mpaauw <matt.paauw@gmail.com>
2019-05-11 17:37:36 -0700

    Format solution.

--- 100-same-tree.cs
+++ 100-same-tree.cs
@@ -1,26 +1,30 @@
-public class Solution {
-    public bool IsSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null)
+public class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == null && q == null)
         {
             return true;
         }
-        
-        if((p == null && q != null) || (p != null && q == null))
+
+        if ((p == null && q != null) || (p != null && q == null))
         {
             return false;
         }
 
-        if(p.val == q.val)
+        if (p.val == q.val)
         {
             return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
         }
-        else{
+        else
+        {
             return false;
         }
     }
 }
 
-public class TreeNode {
+public class TreeNode
+{
     public int val;
     public TreeNode left;
     public TreeNode right;
