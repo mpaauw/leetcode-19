3d7d5a5f37a88886092b75dcd7954e9958d25246
mpaauw <matt.paauw@gmail.com>
2019-05-16 10:44:14 -0700

    Add solution for 7.

---
+++ 7-reverse-integer.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        bool isSigned = (x < 0) ? true : false;
+        int result = 0;
+        x = (x < 0) ? x *= -1 : x;
+        while (x > 0)
+        {
+            if(result > 0 && Int32.MaxValue / result < 10)
+            {
+                return 0;
+            }
+            result *= 10;
+            result += x % 10;
+            x /= 10;
+        }
+        return (isSigned) ? result * -1 : result;
+    }
+}
